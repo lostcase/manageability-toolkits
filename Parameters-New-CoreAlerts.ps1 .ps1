131e2b124609332f6a7d2184a1586c57bc93cf26
lostcase <36469336+lostcase@users.noreply.github.com>
2020-02-17 15:26:09 +0100

    Cleanup and Rename

---
+++ Parameters-New-CoreAlerts.ps1 .ps1
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.\New-CoreAlerts.ps1 `
+-SubscriptionID "<subscriptionId>" `
+-WorkspaceName "<Log Analytics Workspace Name>" `
+-ResourceGroup "<Log Analytics Workspace resource group name>" `
+-Location '<Location of workspace and resource group>' `
+-ExistingActionGroupName '<Name of existing action group>'
